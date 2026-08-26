965521cc3c5f0f12cd3863559fefa0b3fc9ad9db
Sonika Kumar <sonikak004@gmail.com>
2026-08-26 15:19:30 +0530

    Swap out the 6th gallery photo per user request

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -9,7 +9,7 @@ const images = [
   "/gallery/photo-15.jpg",
   "/gallery/photo-16.jpg",
   "/gallery/photo-17.jpg",
-  "/gallery/photo-18.jpg",
+  "/gallery/photo-11.jpg",
 ];
 
 // Tailwind classes for the perfect 4x3 flush bento box
